7fda7389be82019f11463dbc829fd5ffdf583d16
Kevin Oliveira <kevin.oliveira@live.com>
2019-03-16 01:01:01 -0300

    mv silent fail

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ for file in `find . -path ./node_modules -prune -o -name "*.md" -print -type f`;
   markgen -e  "$file"
 done
 
-mv *.html */*.html public/. --force && true
+mv *.html */*.html public/. --force 2> /dev/null
