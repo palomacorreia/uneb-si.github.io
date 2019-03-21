2e398f17f6ffbfc4929e3e3972b6fc2e4c9b6f75
Kevin Oliveira <kevin.oliveira@live.com>
2019-03-20 22:57:06 -0300

    better build process

--- build.sh
+++ build.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+SECONDS=0.0;
 
+echo "Generation Html files from markdown"
 for file in `find ./pages/  -name "*.md" -print -type f`; do
+  echo ">> generating $file"
   markgen -e  "$file"
+  # echo ">> ok"
 done
 
-mv pages/*.html public/. --force --update
+echo ""
+echo "Creating public folder"
+mkdir public >/dev/null 2>&1
+# echo ">> ok"
+
+
+echo ""
+echo "Moving html files to public folder"
+for file in `find ./pages/  -name "*.html" -print -type f`; do
+  echo ">> moving $file"
+  mv "$file" public/. --force --update
+  # echo ">> ok"
+done
+
+echo ""
+echo "Build finished, $SECONDS s"
+
+
+
